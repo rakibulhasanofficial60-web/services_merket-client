61361857792db7c1b85a062bf4ffeae63bf6f62c
Kajal <fhkajal.ouritspace@gmail.com>
2025-11-12 16:37:11 +0600

    map

--- src/routes/pages/Home/Home/Home.jsx
+++ src/routes/pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
+import AddressSelection from "../../Map/Map";
+// import MapComponent from "../../Map/SelectMap";
 import Services from "../../Services/Services";
 
 const Home = () => {
     return (
         <div className="">
             <Services />
+            {/* <MapComponent></MapComponent> */}
+            <AddressSelection />
         </div>
     );
 };
